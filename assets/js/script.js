1381031cb88dcfaafcd8a784f918264285828b06
agungfebry <49956789+agungFebry@users.noreply.github.com>
2020-02-07 04:18:38 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function()
+    {
+    $(".page-scroll").on('click', function(event) 
+        {      
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate(
+            {
+            scrollTop: $(hash).offset().top - 120
+            }, 1000);
+        });
+    
+    // $(window).scroll(function()
+    //     {
+    //     var y = $(window).scrollTop();
+
+    //     if(y >= 585)
+    //         {  
+    //         $('#navbarTest').slideDown(500).css('background-color','#f4f4f4');
+    //         $('#navbarTest').slideDown(500).css('background', '#ffffff');
+    //         }
+    //     else
+    //         {
+    //         $('#navbarTest').css('background-image','none')
+    //         }
+    //     });
+
+    });
