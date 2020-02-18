cccf3438e3fa0ced068e677b5bb3ff9db5f2a733
agungfebry <49956789+agungFebry@users.noreply.github.com>
2020-02-18 19:12:55 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,13 @@ $(document).ready(function()
 
         $('html, body').animate(
             {
-            scrollTop: $(hash).offset().top - 120
+            scrollTop: $(hash).offset().top - 100
             }, 1000);
         });
-    
-    // $(window).scroll(function()
-    //     {
-    //     var y = $(window).scrollTop();
-
-    //     if(y >= 585)
-    //         {  
-    //         $('#navbarTest').slideDown(500).css('background-color','#f4f4f4');
-    //         $('#navbarTest').slideDown(500).css('background', '#ffffff');
-    //         }
-    //     else
-    //         {
-    //         $('#navbarTest').css('background-image','none')
-    //         }
-    //     });
-
     });
+
+$(function()
+{
+  $(".preloader").fadeOut(2000);
+});
+
